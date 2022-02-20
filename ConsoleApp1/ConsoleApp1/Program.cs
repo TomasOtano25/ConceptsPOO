aa63364a52fea6fe0ed92838384bd3d07e445941
tomasotano25 <tomasotano25@gmail.com>
2022-02-20 11:32:25 -0400

    first  commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptsPOO;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+
+Console.WriteLine(new Date(2020, 2, 11));
+Console.WriteLine(new Date(1974, 9, 23));
+Console.WriteLine(new Date(1985, 18, 45));
